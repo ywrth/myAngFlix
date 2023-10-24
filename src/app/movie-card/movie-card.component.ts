35ffc0747a0339fb9a91ea3d6001796fe3263ade
ywrth <zuatworknow@gmail.com>
2023-10-24 21:54:39 +0200

    movie-card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -15,46 +15,21 @@ export class MovieCardComponent implements OnInit {
   movies: any[] = [];
   likedMovies: { [key: string]: boolean } = {};
   hoveredMovieId: string | null = null;
-  userFavoriteMovies: any[] = [];
-
-
+  showModal: boolean = false;
+  currentImageSrc: string = '';
+  
   constructor(
     private fetchApiData: FetchApiDataService,
     private snackBar: MatSnackBar,
     private router: Router,
-    public dialog: MatDialog,
-  ) { }
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-      this.fetchMoviesAndFavorites();
+    this.fetchMoviesAndFavorites();
   }
  
-
-  getMovies(): void {
-    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
-    });
-  }
-
-  // Check if a movie is liked
-  isFavorite(movieId: string): boolean {
-    return this.likedMovies[movieId];
-  }
-
-  // Toggle the favorite status of a movie
-  toggleFavorite(movie: any): void {
-    if (this.isFavorite(movie._id)) {
-      this.removeFromFavorites(movie._id);
-    } else {
-      this.addToFavorites(movie._id);
-    }
-  }
-
-
   fetchMoviesAndFavorites(): void {
-    // Combine both API calls
     forkJoin({
       allMovies: this.fetchApiData.getAllMovies(),
       favoriteMovies: this.fetchApiData.getFavoriteMovies()
@@ -67,11 +42,23 @@ export class MovieCardComponent implements OnInit {
     });
   }
 
-updateLikedMoviesBasedOnFavorites(favoriteMovieIds: string[]): void {
-  for (const id of favoriteMovieIds) {
-    this.likedMovies[id] = true;
+  updateLikedMoviesBasedOnFavorites(favoriteMovieIds: string[]): void {
+    for (const id of favoriteMovieIds) {
+      this.likedMovies[id] = true;
+    }
   }
-}
+
+  isFavorite(movieId: string): boolean {
+    return this.likedMovies[movieId];
+  }
+
+  toggleFavorite(movie: any): void {
+    if (this.isFavorite(movie._id)) {
+      this.removeFromFavorites(movie._id);
+    } else {
+      this.addToFavorites(movie._id);
+    }
+  }
 
   openInfoDialog(title: string, content: string): void {
     this.dialog.open(InfoDialogComponent, {
@@ -82,66 +69,45 @@ updateLikedMoviesBasedOnFavorites(favoriteMovieIds: string[]): void {
   addToFavorites(movieId: string): void {
     const username = localStorage.getItem('username');
     if (!username) return;
-  
-    console.log(`Attempting to add movie with ID ${movieId} to favorites for user ${username}`);  // Log the attempt
-  
     this.fetchApiData.addToFavorites(username, movieId).subscribe({
       next: () => {
         this.likedMovies[movieId] = true;
         this.snackBar.open('Added to favorites!', 'OK', { duration: 2000 });
-        console.log(`Successfully added movie with ID ${movieId} to favorites.`);  // Log the success
       },
       error: (error) => {
         this.snackBar.open(error, 'OK', { duration: 2000 });
-        console.error(`Error adding movie with ID ${movieId} to favorites. Error:`, error);  // Log the error
-      },
+      }
     });
   }
 
-  // Add these properties:
-showModal: boolean = false;
-currentImageSrc: string = '';
-
-// And these methods:
-openImageModal(imageUrl: string): void {
-    this.currentImageSrc = imageUrl;
-    this.showModal = true;
-}
-
-closeImageModal(): void {
-    this.showModal = false;
-}
-  
-
-  // Remove a movie from favorites
   removeFromFavorites(movieId: string): void {
     const username = localStorage.getItem('username');
     if (!username) return;
-
     this.fetchApiData.removeFromFavorites(username, movieId).subscribe({
       next: () => {
         this.likedMovies[movieId] = false;
         this.snackBar.open('Removed from favorites!', 'OK', { duration: 2000 });
       },
-      error: (error) => this.snackBar.open(error, 'OK', { duration: 2000 }),
+      error: (error) => this.snackBar.open(error, 'OK', { duration: 2000 })
     });
+  }
+
+  openImageModal(imageUrl: string): void {
+    this.currentImageSrc = imageUrl;
+    this.showModal = true;
+  }
+
+  closeImageModal(): void {
+    this.showModal = false;
   }
 
   onLogout(): void {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
-    this.router.navigate(['/login']);  // Redirect to the login page.
+    this.router.navigate(['/login']);
   }
 
   hoverIcon(movieId: string, isHovered: boolean): void {
     this.hoveredMovieId = isHovered ? movieId : null;
-  }
-
-  toggleLike(movieId: string): void {
-    if (this.likedMovies[movieId]) {
-      this.likedMovies[movieId] = false;
-    } else {
-      this.likedMovies[movieId] = true;
-    }
   }
 }
